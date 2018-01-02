90a16168da7c6831d3ce5f0c12d764243033bae8
Philipp Stepanenko <finik-st@narod.ru>
2018-01-03 02:52:34 +0300

    fix add registers

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ var args; // arguments
 var step = 1;
 
 function init(){
-	for (var i=0; i<n;i++){
-		document.getElementById('registers').innerHTML+="<tr><td>R"+parseInt(i+1)+"</td><td><input id='r" + parseInt(i) + "' type='number' value='0' min='0'></input></td></tr>";
+	for (var i=0; i<n; i++){
+		//document.getElementById('registers').innerHTML+="<tr><td>R"+parseInt(i+1)+"</td><td><input id='r" + parseInt(i) + "' type='number' value='0' min='0'></input></td></tr>";
+		document.getElementById('registers').innerHTML+="<tr><td>R" + (i + 1) +"</td><td><input id='r" + i + "' type='number' value='0' min='0'></input></td></tr>";
 		}
 }
 
 function load(){
 	// load registers
 	regs = document.getElementsByTagName('input');
-	for (var i=0; i<n;i++){
+	for (var i=0; i<n; i++){
 		registers[i]=parseInt(regs[i].value);
 	}
 	// load program
@@ -39,7 +40,7 @@ function stp(){
 	if(command=="Z") registers[args[0]-1] = 0;
 	else if (command=="S") registers[args[0]-1] ++;
 	else if (command=="T") registers[args[1]-1] = registers[args[0]-1];
-	else if (command=="J" && registers[args[0]-1] == registers[args[1]-1]) step = parseInt(args[2]);
+	else if (command=="J" && registers[args[0]-1] == registers[args[1]-1]) step = args[2];
 	show_regs();
 }
 
@@ -52,18 +53,18 @@ function start(){
 
 function show_regs(){
 	regs = document.getElementsByTagName('input');
-	for (var i=0; i<n;i++){
+	for (var i=0; i<n; i++){
 		regs[i].value=registers[i];
 	}
 }
 
 function con(str){
-	str=(typeof str)=="number" ? "line " + str : str;
+	str = typeof str == "number" ? "line " + str : str;
 	document.getElementById('console').innerHTML="<b>status:</b> "+str;
 	console.log(str);
 }
 
-function add_reg(){
+function add(){
 	n++;
-	document.getElementById('registers').innerHTML+="<tr><td>R"+parseInt(n)+"</td><td><input id='r" + parseInt(n) + "' type='number' value='0' min='0'></input></td></tr>";
+	document.getElementById('registers').innerHTML += "<tr><td>R" + n + "</td><td><input id='r" + n + "' type='number' value='0' min='0'></input></td></tr>";
 }
