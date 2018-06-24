74fa88924f0b6caf217a1353fb6362904bbd59ff
Philipp Stepanenko <finik-st@narod.ru>
2018-06-24 03:42:31 +0300

    delete listeners for buttons

--- script.js
+++ script.js
@@ -6,49 +6,39 @@ let program, command, args;
 let step = 1;
 
 // UI variables
-let b1, b2, b3, b4; // buttons
 let reg, tr, td, label, value; // register tags
 
 // other variables
 let ready = false; // ready flag
-const EMPTY_STRING = "";
+const EMPTY = "";
+const DEFAULT_N = 7;
 
+// initialization
 function init(){
-	b1 = document.getElementById("load");
-	b2 = document.getElementById("step");
-	b3 = document.getElementById("start");
-	b4 = document.getElementById("add");
-	reg = document.getElementById("registers");
-
-	b1.addEventListener("click", load, true);
-	b2.addEventListener("click", current_step, true);
-	b3.addEventListener("click", start, true);
-	b4.addEventListener("click", add, false);
-
-	for (let i = 0; i < 7; i++){
+	for (let i = 0; i < DEFAULT_N; i ++){
 			add();
 			}
 }
 
-// load program
+// loading
 function load(){
-	// load registers
+	// registers
 	regs = document.getElementsByTagName('input');
 	for (let i = 0; i < n; i++){
-		if (regs[i].value == EMPTY_STRING){
+		if (regs[i].value == EMPTY){
 			regs[i].value = 0;
 			document.getElementById('r' + (i + 1)).value = regs[i].value;
 		}
 		registers[i] = parseInt(regs[i].value);
 	}
-	// load program
+	// program
 	program = document.getElementById('program').value.split("\n");
 	step = 1;
-	// show step
 	cons("loaded");
 	ready = true;
 }
 
+// current step
 function current_step(){
 	if (ready == true){
 		if (program.length <= step - 1){
@@ -61,16 +51,16 @@ function current_step(){
 		}
 		command = program[step - 2].split(' ')[0].toUpperCase();
 		args = program[step - 2].substr(program[step-2].search(' ') + 1).split(' ');
-		if(command == "Z" && args.length == 1 && args[0] != EMPTY_STRING){
+		if(command == "Z" && args.length == 1 && args[0] != EMPTY){
 			registers[args[0] - 1] = 0;
 		}
-		else if (command == "S" && args.length == 1 && args[0] != EMPTY_STRING){
+		else if (command == "S" && args.length == 1 && args[0] != EMPTY){
 					registers[args[0] - 1] ++;
 		}
-		else if (command == "T" && args.length == 2 && (args[0] != EMPTY_STRING || args[1] != EMPTY_STRING)){
+		else if (command == "T" && args.length == 2 && (args[0] != EMPTY || args[1] != EMPTY)){
 					registers[args[1] - 1] = registers[args[0]-1];
 		}
-		else if (command == "J" && args.length == 3 && (args[0] != EMPTY_STRING || args[1] != EMPTY_STRING || args[2] != EMPTY_STRING)) {
+		else if (command == "J" && args.length == 3 && (args[0] != EMPTY || args[1] != EMPTY || args[2] != EMPTY)) {
 						if (registers[args[0] - 1] == registers[args[1] - 1]) step = parseInt(args[2]);
 					}
 		else {
@@ -81,12 +71,14 @@ function current_step(){
 	}
 }
 
+// error
 function error(){
 	ready = false;
 	cons("error in line " + parseInt(step - 1));
 	step = program.length + 2;
 }
 
+// execution by steps
 function start(){
 	if (ready == true){
 		while (program.length > step - 1){
@@ -96,6 +88,7 @@ function start(){
 	}
 }
 
+// show registers
 function show_regs(){
 	regs = document.getElementsByTagName('input');
 	for (let i = 0; i < n; i ++){
@@ -103,12 +96,13 @@ function show_regs(){
 	}
 }
 
+// console
 function cons(str){
 	str = typeof str == "number" ? "line " + str : str;
 	document.getElementById('console').innerHTML = `<b>Status:</b> ${str}`;
-	//console.log(str);
 }
 
+// new register
 function add(){
 	n ++;
 	reg = document.getElementById('registers');
